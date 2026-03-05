d13e8b5b46a7847904ea6a27a67ed7fd30b8c47f
rhonesegara <rhonesegara78@gmail.com>
2026-03-05 21:53:51 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+{
+  "name": "Iloilo Travel Route",
+  "short_name": "TravelRoute",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#e0f7fa",
+  "theme_color": "#0066ff",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
